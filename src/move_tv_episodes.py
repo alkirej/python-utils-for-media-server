a547853e71994fe1de567c85781955e9f31d8851
Jeffery Alkire <jeff@alkire.net>
2024-02-18 19:08:29 -0800

    Add .mkv onto end of file to move.

--- src/move_tv_episodes.py
+++ src/move_tv_episodes.py
@@ -2,14 +2,14 @@ import glob
 import os
 import shutil
 
-RECORDINGS_DIR: str = "/home/jeff/Videos/recordings/episodes"
+RECORDINGS_DIR: str = "/media/jeff/ToolsDisk/recordings/episodes"
 PLEX_DIR_FOR_TV: str = "/nfs/Media-01/media-store/Video/Television Shows"
 
 
 def process_episode(tv_show_dir_name: str, season_dir_name: str, file_name: str) -> None:
     # file name should not include the extension.  (Mkv will be added automatically)
     season_path: str = os.path.join(tv_show_dir_name, season_dir_name)
-    episode_path: str = os.path.join(season_path, file_name)
+    episode_path: str = os.path.join(season_path, f"{file_name}.mkv")
     episode_files: [str] = glob.glob(f"{episode_path}.*")
 
     if len(episode_files) == 1:
@@ -18,6 +18,7 @@ def process_episode(tv_show_dir_name: str, season_dir_name: str, file_name: str)
         dest_path: str = os.path.join(PLEX_DIR_FOR_TV, season_path, f"{file_name}.mkv")
         shutil.move(episode_path, dest_path)
         print("Complete.")
+
 
 def process_season(tv_show_dir_name: str, season_dir_name: str) -> None:
     season_dir: str = os.path.join(tv_show_dir_name, season_dir_name)
