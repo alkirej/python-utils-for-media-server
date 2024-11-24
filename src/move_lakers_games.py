fe2b4479f526c8b56c29dd09920f23a41a3dec5f
Jeffery Alkire <jeff@alkire.net>
2024-11-23 20:47:34 -0800

    fix type

--- src/move_lakers_games.py
+++ src/move_lakers_games.py
@@ -15,7 +15,7 @@ PLEX_DIR_FOR_GAMES: dict = {"1962": "/nfs/Media-01/media-store/Video/Sports Game
                             "2017": "/nfs/Media-04/media-store/Video/Sports Games/Lakers/2016-17",
                             "2018": "/nfs/Media-04/media-store/Video/Sports Games/Lakers/2017-18",
                             "2019": "/nfs/Media-04/media-store/Video/Sports Games/Lakers/2018-19",
-                            "2020": "/nfs/Media-04/media-store/Video/Sports Games/Lakers/2019-20"
+                            "2020": "/nfs/Media-04/media-store/Video/Sports Games/Lakers/2019-20",
                             "2021": "/nfs/Media-04/media-store/Video/Sports Games/Lakers/2020-21"
                             }
 GAME_POSTERS: dict = {"1962": "game-poster.webp",
@@ -30,7 +30,7 @@ GAME_POSTERS: dict = {"1962": "game-poster.webp",
                       "2017": "game-poster.webp",
                       "2018": "game-poster.webp",
                       "2019": "game-poster.jpg",
-                      "2020": "game-poster.webp"
+                      "2020": "game-poster.webp",
                       "2021": "game-poster.webp"
                       }
 
